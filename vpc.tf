3dea3fbe1d5b0191a12daf47335383ea8619fedb
root <root@ip-172-31-35-54.ap-south-1.compute.internal>
2025-08-08 14:08:36 +0000

    vpc changes

--- vpc.tf
+++ vpc.tf
@@ -38,7 +38,7 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.my_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw
+    gateway_id = aws_internet_gateway.my_igw.id
   }
   tags = {
     Name = "public-route-table"
